ea1a902da0b0a957172ae0107780ad85cc0360d0
Eric Earl <eric.earl@nih.gov>
2024-07-01 16:32:48 -0400

    Update swarm.sh
    Overhauled the swarm.sh a bit

--- swarm.sh
+++ swarm.sh
@@ -1,23 +1,42 @@
 #! /bin/bash
 
+# initialize inputs
+ABCD_FASTQC01=/data/NIMH_scratch/zwallymi/earlea-d2b/fastqc/20240501_abcd_fastqc01.txt
 BIDS_OUTPUT_DIR=/data/NIMH_scratch/zwallymi/earlea-d2b/downloads/testing_chain
-CODE_DIR=/data/NIMH_scratch/zwallymi/earlea-d2b/fasttrack2bids
+MCR91_DIR=/data/NIMH_scratch/zwallymi/earlea-d2b/abcd-dicom2bids/env_setup/MCR_v9.1/v91
 NDA_PACKAGE_ID=1228006
-S3LINKS_DIR=/data/NIMH_scratch/zwallymi/earlea-d2b/downloads/20240501_abcd_fastqc01_all_p-20_s-25
-SWARM_LOG_DIR=/data/NIMH_scratch/zwallymi/earlea-d2b/logs/fasttrack2bids
+SESSIONS_CSV=/data/NIMH_scratch/zwallymi/earlea-d2b/downloads/small_sessions.csv
+LOG_BASEDIR=/data/NIMH_scratch/zwallymi/earlea-d2b/logs
 
-mkdir -p $SWARM_LOG_DIR
-OUTPUT_BASENAME=`basename ${BIDS_OUTPUT_DIR}`
-cp ${CODE_DIR}/header.swarm ${SWARM_LOG_DIR}/${OUTPUT_BASENAME}.swarm
+# cleanup pre-run to allow all files to be downloaded
+echo "### Cleaning out the download progress file to allow all files to be downloaded ###"
+rm -rf ~/NDA/nda-tools/downloadcmd/packages/${NDA_PACKAGE_ID}/.download-progress/*
 
-echo "#SWARM --logdir ${SWARM_LOG_DIR}" >> ${SWARM_LOG_DIR}/${OUTPUT_BASENAME}.swarm
+# run the fasttrack2s3.py script
+echo "### Running the fasttrack2s3.py script ###"
 
-for LINK in ${S3LINKS_DIR}/*_s3links.txt ; do
+CODE_DIR=$(readlink -f `dirname $0`)
+TEMP_BASENAME=`date '+%Y-%m-%d'`_`head /dev/urandom | tr -dc A-Z1-9 | head -c8`
+LOG_DIR=${LOG_BASEDIR}/${TEMP_BASENAME}
+mkdir -p $LOG_DIR
+poetry run --directory ${CODE_DIR} python ${CODE_DIR}/fasttrack2s3.py -sep -csv ${SESSIONS_CSV} ${ABCD_FASTQC01} ${LOG_DIR}
+
+# create the swarm file
+echo "### Creating the swarm file ###"
+
+BIDS_OUTPUT_BASENAME=`basename ${BIDS_OUTPUT_DIR}`
+SWARM_FILE=${LOG_DIR}/${BIDS_OUTPUT_BASENAME}.swarm
+cp ${CODE_DIR}/header.swarm ${SWARM_FILE}
+echo "#SWARM --logdir ${LOG_DIR}" >> ${SWARM_FILE}
+
+# for each s3links file (each separated session) in the LOG_DIR, run the pipeline, bids_corrections, and rsync back
+for LINK in ${LOG_DIR}/*/*_s3links.txt ; do
     CMD1="poetry run --directory ${CODE_DIR} python ${CODE_DIR}/pipeline.py -p ${NDA_PACKAGE_ID} -c ${CODE_DIR}/dcm2bids_v3_config.json -z LOGS BIDS --n-download 2 --n-unpack 2 --n-convert 1 -o /lscratch/\${SLURM_JOB_ID} -s ${LINK}"
-    CMD2="poetry run --directory ${CODE_DIR} python ${CODE_DIR}/bids_corrections.py -b /lscratch/\${SLURM_JOB_ID}/rawdata -t /lscratch/\${SLURM_JOB_ID} --dwiCorrectOldGE --funcfmapIntendedFor /data/NIMH_scratch/zwallymi/earlea-d2b/abcd-dicom2bids/env_setup/MCR_v9.1/v91"
+    CMD2="poetry run --directory ${CODE_DIR} python ${CODE_DIR}/bids_corrections.py -b /lscratch/\${SLURM_JOB_ID}/rawdata -t /lscratch/\${SLURM_JOB_ID} --dwiCorrectOldGE --funcfmapIntendedFor ${MCR91_DIR}"
     CMD3="for BIDS in code rawdata sourcedata ; do if [ -d /lscratch/\${SLURM_JOB_ID}/\${BIDS} ] ; then rsync -art /lscratch/\${SLURM_JOB_ID}/\${BIDS} ${BIDS_OUTPUT_DIR}/ ; fi ; done"
 
-    echo "${CMD1} ; ${CMD2} ; echo rsyncing data back from /lscratch/\${SLURM_JOB_ID} ; ${CMD3} ; echo rsync completed to ${BIDS_OUTPUT_DIR}" >> ${SWARM_LOG_DIR}/${OUTPUT_BASENAME}.swarm
+    echo "${CMD1} ; ${CMD2} ; echo rsyncing data back from /lscratch/\${SLURM_JOB_ID} ; ${CMD3} ; echo rsync completed to ${BIDS_OUTPUT_DIR}" >> ${SWARM_FILE}
 done
 
-echo "swarm ${SWARM_LOG_DIR}/${OUTPUT_BASENAME}.swarm"
+echo "### The following command is printed for your convenience, but not run yet ###"
+echo "swarm ${SWARM_FILE}"
